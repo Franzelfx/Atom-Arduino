1e104f037bbe53d9048627138b881c8d9c810d7c
Franzelfx <fabian.franz0596@gmail.com>
2019-04-25 21:55:48 +0200

    Frequenzy Counter Arduino Nano

---
+++ ESPFillSensor/src/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * ESP-Wasserstandssensor Software 2.0
+ * @autor Fabian Franz
+ */
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <PubSubClientOverload.h>
+#include <EEPROM.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266mDNS.h>
+
+void setup() {
+	// put your setup code here, to run once:
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+}
---
+++ FreqCounter/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+//------------------------
+// Impulszähler
+// Gibt die Impulse pro Sekunde
+// des Spannungsignals an Pin 7 aus
+//-------------------------
+int pin = 7;
+unsigned long N;          //Anzahl der Impulse
+unsigned long T;          //Zeitintervall in us
+unsigned long time;       //Startzeit
+
+void setup(){
+	Serial.begin(9600);
+	pinMode(pin, INPUT);
+	T = 1e6;
+}
+
+void loop(){
+	N = 0;
+	time = micros();
+	do {
+		if (pulseIn(pin, HIGH)>0) N++;
+	}
+	while( micros() < (time+T) );
+	Serial.println(N);
+}
